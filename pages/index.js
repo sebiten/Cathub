6076b013ebc1ec5c3ca95c1385bd5596cd31a48f
Sebiten <sebdevspace@gmail.com>
2023-01-12 21:04:32 -0300

    beg

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,13 @@
 import Image from "next/image";
 import { useState } from "react";
+import FavoriteImage from "../components/FavBreed";
 import Navbar from "../components/Navbar";
 
-function FavoriteImage({ breed }) {
-  const [isFavorited, setIsFavorited] = useState(false);
 
-  function handleClick() {
-    setIsFavorited(!isFavorited);
-  }
-
-  return (
-    <div className="relative">
-      <svg
-        className="absolute top-0 right-0 m-2 cursor-pointer"
-        onClick={handleClick}
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill={isFavorited ? '#F44336' : 'none'}
-        stroke={isFavorited ? '#F44336' : '#000'}
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-      </svg>
-    </div>
-  );
-}
 export default function Home({ data }) {
+  const [search, setSearch] = useState("hound");
+  const [breed, setBreed] = useState("african");
+
   return (
         <div>
       <div>
@@ -38,7 +17,7 @@ export default function Home({ data }) {
           type="text"
           className="border bg-slate-200 border-gray-400 rounded w-full p-2"
           placeholder="Search your favorite breed"
-          value={""}
+          defaultValue={breed}
           onChange={(e) => setSearch(e.target.value)}
         />
       </div>
@@ -54,7 +33,7 @@ export default function Home({ data }) {
                 <img
                   src={item}
                   alt="Picture of theuthor"
-                  className="w-full h-full object-cover "
+                  className="w-full h-full object-cover border-none"
                 />
               </div>
             );
@@ -64,7 +43,7 @@ export default function Home({ data }) {
   );
 }
 export async function getStaticProps() {
-  const res = await fetch(`https://dog.ceo/api/breed/hound/images`);
+  const res = await fetch(`https://dog.ceo/api/breed/${search}/images`);
   const data = await res.json();
   return {
     props: {
