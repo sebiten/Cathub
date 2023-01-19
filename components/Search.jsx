5c788c626bd66b82498adad5c9948257b030039e
Sebiten <sebdevspace@gmail.com>
2023-01-19 20:14:13 -0300

    almost search done

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,38 +1,29 @@
-import useDogs from "../hooks/useDogs";
+// import useDogs from "../hooks/useDogs";
 
-function Search() {
-  const [search, setSearch] = React.useState("");
-  const { dogs, setDogs } = useDogs();
+// function Search() {
+//   const [search, setSearch] = React.useState("");
+//   const [results, setResults] = React.useState([]);
+//   const { searchBreed } = useDogs();
 
-  const searchBreed = async () => {
-    try {
-      const response = await fetch(
-        `https://dog.ceo/api/breed/search?q=${search}`
-      );
-      const data = await response.json();
-      setDogs(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
-  return (
-         <form
-         onSubmit={(e) => {
-          e.preventDefault();
-          searchBreed();
-        }
-      }
-      >
-        <input
-          type="text"
-          className="border bg-slate-200 border-gray-400 rounded w-full p-2"
-          placeholder="Search your favorite breed"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </form> 
-  )
-}
+ 
+//   return (
+//          <form
+//          onSubmit={(e) => {
+//           e.preventDefault();
+//           searchBreed();
+//         }
+//       }
+//       >
+//         <input
+//           type="text"
+//           className="border bg-slate-200 border-gray-400 rounded w-full p-2"
+//           placeholder="Search your favorite breed"
+//           value={search}
+//           onChange={(e) => setSearch(e.target.value)}
+//         />
+//       </form> 
+//   )
+// }
 
-export default Search
+// export default Search
--- pages/index.js
+++ pages/index.js
@@ -6,36 +6,79 @@ import Navbar from "../components/Navbar";
 import SectionBreeds from "../components/SectionBreeds";
 
 export default function Home() {
-
+  const [results, setResults] = useState([]);
   const [search, setSearch] = useState("");
-  
+  const [searched, setSearched] = useState(false);
+
+  useEffect(() => {
+    async function fetchBreeds() {
+      const res = await fetch(
+        `https://api.thedogapi.com/v1/breeds/search?q=${search}`
+      );
+      const data = await res.json();
+      setResults(data);
+      console.log(data);
+    }
+    setSearched(false);
+    fetchBreeds();
+  }, [search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setSearched(true);
+  };
+
   return (
-    
-        <div>
+    <div>
       <div>
-      <Navbar />
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          setSearch("");
-        }
-      }
-      >
-        <input
-          type="text"
-          className="border bg-slate-200 border-gray-400 rounded w-full p-2"
-          placeholder="Search your favorite breed"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </form>
+        <Navbar />
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            className="border bg-slate-200 border-gray-400 rounded w-full p-2"
+            placeholder="Search your favorite breed"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-slate-200 hover:bg-slate-300 text-gray-800 font-bold py-2 px-4 rounded"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+      <div>
+        {searched && (
+          <div>
+            {results.map((result) => (
+              <div key={result.id}>
+                <Link href={`/breed/${result.name}`}>
+                  <div className="flex flex-col items-center justify-center">
+                    <div className="flex flex-col items-center justify-center">
+                      <Image
+                        src={`https://cdn2.thedogapi.com/images/${result.reference_image_id}.jpg`}
+                        alt={result.name}
+                        width={300}
+                        height={300}
+                      />
+
+                      <h2 className="text-2xl font-bold my-4">{result.name}</h2>
+                      <p className="text-gray-500">{result.temperament}</p>
+                      <p className="text-gray-500">{result.origin}</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      {searched && <SectionBreeds />}
+      
     </div>
-        <SectionBreeds/>
-      </div>    
   );
 }
-
-
-
-
-
