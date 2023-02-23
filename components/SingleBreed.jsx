a2e88959394719957f3d171ff2a643ab0b3d2c85
Sebiten <sebdevspace@gmail.com>
2023-02-23 15:32:59 -0300

    responsive finished

--- components/SingleBreed.jsx
+++ components/SingleBreed.jsx
@@ -63,65 +63,66 @@ function SingleBreed({
       <div>
         <Navbar />
       </div>
-      <div className="grid object-center place-items-center mt-10 w-full md:w-1/2 mx-auto p-4 py-8">
-  <Image
-    src={`https://cdn2.thedogapi.com/images/${image}.jpg`}
-    alt={name}
-    width={600}
-    height={500}
-    className="rounded-lg"
-    onClick={() => setIsOpen(true)}
-  />
-  <div className="flex flex-col w-full md:w-1/2">
-    <div className="flex justify-center flex-col">
-      <h1 className="text-3xl font-bold uppercase my-5">
-        {name}
-      </h1>
-      <p className="text-lg w-full">
-        <span className="font-bold">Temperament:</span> {temperament}
-      </p>
-      <p className="text-lg w-full">
-        <span className="font-bold">{origin ? `Origin: ` : ""}</span>
-        {origin}
-      </p>
-      <p className="text-lg w-full">
-        <span className="font-bold">
-          {life_span ? `Life span: ` : ""}
-        </span>
-        {life_span}
-      </p>
-      <p className="text-lg w-full">
-        <span className="font-bold">{breedfor ? `Breed for: ` : ""}</span>
-        {breedfor}
-      </p>
-      <p className="text-lg w-full">
-        <span className="font-bold">
-          {breed_group ? `Breed group: ` : ""}
-        </span>
-        {breed_group}
-      </p>
-      <p className="text-lg w-full">
-        <span className="font-bold">
-          {description ? `Description: ` : ""}
-        </span>
-        {description}
-      </p>
-      <p className="text-lg w-full">
-        <span className="font-bold">
-          {width ? `Width: ` : ""}
-        </span>
-        {width}
-      </p>
-      <p className="text-lg w-full">
-        <span className="font-bold">
-          {height ? `Height: ` : ""}
-        </span>
-        {height}
-      </p>
-    </div>
-    <div className="flex flex-col md:flex-row items-center justify-center md:justify-between gap-8 mt-6">
+      <div class="grid place-items-center mt-10 mx-auto p-4 py-8 max-w-screen-md lg:grid-cols-2">
+  <div class="max-w-md mx-auto">
+    <Image
+      width={500}
+      height={500}
+      src={`https://cdn2.thedogapi.com/images/${image}.jpg`}
+      alt={name}
+      class="w-full h-full object-cover rounded-lg cursor-pointer"
+      onClick={() => setIsOpen(true)}
+    />
+  </div>
+  <div class="flex flex-col justify-center max-w-xl mx-auto mt-8 lg:mt-0 lg:ml-8">
+    <h1 class="text-3xl font-bold uppercase my-5 text-center lg:text-left">
+      {name}
+    </h1>
+    <p class="text-lg w-full">
+      <span class="font-bold">Temperament:</span> {temperament}
+    </p>
+    <p class="text-lg w-full">
+      <span class="font-bold">{origin ? `Origin: ` : ""}</span>
+      {origin}
+    </p>
+    <p class="text-lg w-full">
+      <span class="font-bold">
+        {life_span ? `Life span: ` : ""}
+      </span>
+      {life_span}
+    </p>
+    <p class="text-lg w-full">
+      <span class="font-bold">{breedfor ? `Breed for: ` : ""}</span>
+      {breedfor}
+    </p>
+    <p class="text-lg w-full">
+      <span class="font-bold">
+        {breed_group ? `Breed group: ` : ""}
+      </span>
+      {breed_group}
+    </p>
+    <p class="text-lg w-full">
+      <span class="font-bold">
+        {description ? `Description: ` : ""}
+      </span>
+      {description}
+    </p>
+    <p class="text-lg w-full">
+      <span class="font-bold">
+        {width ? `Width: ` : ""}
+      </span>
+      {width}
+    </p>
+    <p class="text-lg w-full">
+      <span class="font-bold">
+        {height ? `Height: ` : ""}
+      </span>
+      {height}
+    </p>
+
+    <div class="flex flex-col lg:flex-row justify-center items-center gap-8 mt-6">
       <Link
-        className="text-white bg-sky-700 hover:bg-sky-800 focus:outline-none focus:ring-4 focus:ring-sky-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 md:mb-0 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-900"
+        class="text-white bg-sky-700 hover:bg-sky-800 focus:outline-none focus:ring-4 focus:ring-sky-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-900 lg:mr-4"
         href={`https://en.wikipedia.org/wiki/${name}`}
         target="_blank"
       >
@@ -129,7 +130,7 @@ function SingleBreed({
       </Link>
       <button
         disabled={isFavorited}
-        className="text-black bg-transparent hover:bg-sky-700 focus:outline-none focus:ring-4 focus:ring-sky-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 md:mb-0 dark:bg-transparent dark:hover:bg-sky-700 dark:focus:ring-sky-900"
+        class="text-black bg-transparent hover:bg-sky-700 focus:outline-none focus:ring-4 focus:ring-sky-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-transparent dark:hover:bg-sky-700 dark:focus:ring-sky-900 lg:ml-4"
         onClick={handleFavoriteClick}
       >
         {isFavorited ? "Favorited!" : "Add to favorites"}
